38f4310167af91d4272a6c7541b7c72055af86de
Arian Gohari <a.gohari@startmunich.de>
2024-08-23 09:57:43 +0200

    small readability fix to the Javadoc comments

--- src/main/java/de/gohari/chat_with_paper/controller/PaperController.java
+++ src/main/java/de/gohari/chat_with_paper/controller/PaperController.java
@@ -21,105 +21,110 @@ import java.util.NoSuchElementException;
 @RequiredArgsConstructor
 public class PaperController {
 
-    private final Logger logger = LoggerFactory.getLogger(PaperController.class);
+  private final Logger logger = LoggerFactory.getLogger(PaperController.class);
 
-    private final PaperService paperService;
+  private final PaperService paperService;
 
-    /**
-     * Upload a paper
-     * @param file the paper file
-     * @return the uploaded paper
-     */
-    @PostMapping("/papers")
-    public ResponseEntity<Paper> upload(@RequestParam("file") MultipartFile file) {
-        logger.info("upload | {}", file.getOriginalFilename());
+  /**
+   * Upload a paper
+   *
+   * @param file the paper file
+   * @return the uploaded paper
+   */
+  @PostMapping("/papers")
+  public ResponseEntity<Paper> upload(@RequestParam("file") MultipartFile file) {
+    logger.info("upload | {}", file.getOriginalFilename());
 
-        // Try to save the paper and return HTTP ok if successful
-        try {
-            var paper = paperService.save(file);
-            return ResponseEntity.ok().body(paper);
-        }
-
-        // Return HTTP internal server error if failed
-        catch (IOException e) {
-            logger.error(e.getMessage());
-            return ResponseEntity.internalServerError().build();
-        }
+    // Try to save the paper and return HTTP ok if successful
+    try {
+      var paper = paperService.save(file);
+      return ResponseEntity.ok().body(paper);
     }
 
-    /**
-     * Download a paper
-     * @param id the paper id
-     * @return the paper file
-     */
-    @GetMapping("/papers/{id}")
-    public ResponseEntity<Resource> download(@PathVariable("id") String id) {
-        logger.info("download | {}", id);
+    // Return HTTP internal server error if failed
+    catch (IOException e) {
+      logger.error(e.getMessage());
+      return ResponseEntity.internalServerError().build();
+    }
+  }
 
-        // Try to load the file for given paper id and return it with HTTP ok if successful
-        try {
+  /**
+   * Download a paper
+   *
+   * @param id the paper id
+   * @return the paper file
+   */
+  @GetMapping("/papers/{id}")
+  public ResponseEntity<Resource> download(@PathVariable("id") String id) {
+    logger.info("download | {}", id);
 
-            // Load input stream from the file and create input stream resource
-            var inputStream = paperService.load(id);
-            var inputStreamResource = new InputStreamResource(inputStream);
+    // Try to load the file for given paper id and return it with HTTP ok if
+    // successful
+    try {
 
-            // Create HTTP headers and set accordingly
-            var httpHeaders = new HttpHeaders();
-            httpHeaders.setContentLength(inputStream.available());
-            httpHeaders.setContentType(MediaType.APPLICATION_PDF);
+      // Load input stream from the file and create input stream resource
+      var inputStream = paperService.load(id);
+      var inputStreamResource = new InputStreamResource(inputStream);
 
-            // Return HTTP ok with the file
-            return ResponseEntity.ok().headers(httpHeaders).body(inputStreamResource);
-        }
+      // Create HTTP headers and set accordingly
+      var httpHeaders = new HttpHeaders();
+      httpHeaders.setContentLength(inputStream.available());
+      httpHeaders.setContentType(MediaType.APPLICATION_PDF);
 
-        // Return HTTP not found if the paper with given id is not found
-        catch (NoSuchElementException e) {
-            logger.error(e.getMessage());
-            return ResponseEntity.notFound().build();
-        }
-
-        // Return HTTP internal server error if failed due to IO exception
-        catch (IOException e) {
-            logger.error(e.getMessage());
-            return ResponseEntity.internalServerError().build();
-        }
+      // Return HTTP ok with the file
+      return ResponseEntity.ok().headers(httpHeaders).body(inputStreamResource);
     }
 
-    /**
-     * Download the thumbnail of a paper
-     * @param paperId the paper id
-     * @return the thumbnail file
-     */
-    @GetMapping("/papers/{paperId}/thumbnail")
-    public ResponseEntity<Resource> downloadThumbnail(@PathVariable("paperId") String paperId) {
-        logger.info("downloadThumbnail | {}", paperId);
-
-        // Try to load the thumbnail for given paper id and return it with HTTP ok if successful
-        try {
-
-            // Load input stream from the thumbnail and create input stream resource
-            var inputStream = paperService.loadThumbnail(paperId);
-            var inputStreamResource = new InputStreamResource(inputStream);
-
-            // Create HTTP headers and set accordingly
-            var httpHeaders = new HttpHeaders();
-            httpHeaders.setContentLength(inputStream.available());
-            httpHeaders.setContentType(MediaType.IMAGE_PNG);
-
-            // Return HTTP ok with the thumbnail
-            return ResponseEntity.ok().headers(httpHeaders).body(inputStreamResource);
-        }
-
-        // Return HTTP not found if the paper with given id is not found
-        catch (NoSuchElementException e) {
-            logger.error(e.getMessage());
-            return ResponseEntity.notFound().build();
-        }
-
-        // Return HTTP internal server error if failed due to IO exception
-        catch (IOException e) {
-            logger.error(e.getMessage());
-            return ResponseEntity.internalServerError().build();
-        }
+    // Return HTTP not found if the paper with given id is not found
+    catch (NoSuchElementException e) {
+      logger.error(e.getMessage());
+      return ResponseEntity.notFound().build();
     }
+
+    // Return HTTP internal server error if failed due to IO exception
+    catch (IOException e) {
+      logger.error(e.getMessage());
+      return ResponseEntity.internalServerError().build();
+    }
+  }
+
+  /**
+   * Download the thumbnail of a paper
+   *
+   * @param paperId the paper id
+   * @return the thumbnail file
+   */
+  @GetMapping("/papers/{paperId}/thumbnail")
+  public ResponseEntity<Resource> downloadThumbnail(@PathVariable("paperId") String paperId) {
+    logger.info("downloadThumbnail | {}", paperId);
+
+    // Try to load the thumbnail for given paper id and return it with HTTP ok if
+    // successful
+    try {
+
+      // Load input stream from the thumbnail and create input stream resource
+      var inputStream = paperService.loadThumbnail(paperId);
+      var inputStreamResource = new InputStreamResource(inputStream);
+
+      // Create HTTP headers and set accordingly
+      var httpHeaders = new HttpHeaders();
+      httpHeaders.setContentLength(inputStream.available());
+      httpHeaders.setContentType(MediaType.IMAGE_PNG);
+
+      // Return HTTP ok with the thumbnail
+      return ResponseEntity.ok().headers(httpHeaders).body(inputStreamResource);
+    }
+
+    // Return HTTP not found if the paper with given id is not found
+    catch (NoSuchElementException e) {
+      logger.error(e.getMessage());
+      return ResponseEntity.notFound().build();
+    }
+
+    // Return HTTP internal server error if failed due to IO exception
+    catch (IOException e) {
+      logger.error(e.getMessage());
+      return ResponseEntity.internalServerError().build();
+    }
+  }
 }
